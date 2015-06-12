410ba2b033bba5aa6eb7d9ed241e7a87a6316fd8
Logan Butler <logan@terum.us>
2015-06-11 21:48:58 -0400

    Updated

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtm11/DTD/xhtml-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<meta content="personal purposes"/>
+			<meta charset="utf-8"/>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+			<meta http-equiv="X-UA-Compatible" content="chrome=1, IE=edge">
+			
+			<meta name="description" content="an actual website by logan.. "/>
+			<meta name="keywords" content="personal, homepage, terum"/>
+			<meta name="robots" content="googlebot, bingbot"/>
+			
+			<!-- Cool JavaScripts -->
+			<script type="text/javascript" src="main.js"/></script>
+
+			<!-- Page fonts: Abel, Sans -->
+			<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+			<link href="css/main.one.css" rel="stylesheet" type="text/css"/>
+			<link href="css/main.two.css" rel="stylesheet" type="text/css"/>
+			
+			<!-- Font Awesome 4.3.0 -->
+			<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+		
+			<title>Terum International Solutions</title>
+		</head>
+		
+		<body id="content">
+			<!-- Navigation Sidebar -->
+			<div class="sidebar">
+				<!-- Logo -->
+				<img class="logo" src="http://terum.us/img/teurm.png" height="150" width="180" alt="logo"/>
+				
+				<!-- Navigation -->
+				<div class="left-nav">
+					<nav class="navbar">
+						<ul>
+							<a href="http://terum.us/index.php"><li id="home">Homepage</li></a>
+							<a href="https://github.com/loganbutlre"><li id="repo">Repository</li></a>
+							<a href="http://terum.us/about.php"><li id="abou">About</li></a>
+							<a href="http://terum.us/projects.php"><li id="exra">Projects</li></a>
+							<a href="http://terum.us/contact.php"><li id="cont">Contact</li></a>
+						</ul>
+					</nav>
+				</div>
+				
+				<div class="footer">
+					&copy; 2015 Logan Butler <br /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a class="dl" href="http://terum.us/info.php?action=dmca">DMCA</a>
+				</div>
+			</div>
+			<!-- End Nav-Sidebar -->
+			
+			<!-- Start Body -->
+			<div class="body">
+				<div class="content">
+					<div class="post">
+						<h2>Contact</h2> <br clear="left"/>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24566.634719989594!2d-86.01717261077788!3d39.67605336497421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x944085a42fb1647b!2sFranklin+Central+High+School!5e0!3m2!1sen!2sus!4v1434072680196" width="800" height="275" frameborder="0" style="border:0"></iframe> <p class="cap">*this is where I attend highschool</p><br clear="left"/> <br />
+						<h2>General contact information</h2>
+						<p>please refrain from prank calls and sending dildo shipments to my house</p> <br />
+						
+						<ul>
+							<li>Phone: +1 (703) 853-2534</li><br />
+							<li>Email: logan@terum.us | logan@ragebots.com</li><br />
+							<li>Fax: +1 (800) 45-TERUM </li><br />
+						</pre>
+					</div>
+				</div>
+			</div>
+			<!-- End Body -->
+		</body>
+	</html>
+			
