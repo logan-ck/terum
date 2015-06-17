ba06a81abf0b3fc1ac1d319ef6d39423542faf83
Logan Butler <logan@terum.us>
2015-06-17 17:31:58 -0400

    Updated
    + added post about new flash tech to main index

--- index.php
+++ index.php
@@ -69,6 +69,28 @@
 					</div>
 					<hr />
 					<div class="post">
+						<h2>Crucial shows off new 16nm TLC flash for future SSDs at Computex</h2>
+						<p class="sub-title">computing is getting faster and faster each conference.</p>
+						
+						<br clear="left"/>
+						<img src="img/crucal.jpg" height="300" width="500"/>
+						<br />
+						<p class="page-content"
+						> Crucial didn’t have any new SSDs to show off at Computex this year, but the memory maker did have something enticing on display: a new 16nm TLC NAND flash module from parent company Micron, coming to its solid state drives in the future.</p>
+						<br />
+						<p class="page-content"
+						> Crucial made a big splash in the SSD market when it launched the MX100, a SATA SSD fast enough to compete with Samsung’s popular 840 EVO, but at an even lower price. In 2015, Crucial followed that release with the faster, all-around better MX200, and a new drive, the BX100, with a cheap price tag. The BX100 is our choice for the best budget SSD. Moving to TLC flash will allow Crucial to price its drives even more cheaply.</p>
+						<br />
+						<p class="page-content"
+						> As we explained in our best SSD guide, there are three main types of flash NAND: SLC, MLC and TLC. Most SATA SSDs, including Crucial’s, use 2-bit MLC memory. Those two bits represent four possible binary states, triggered by four different levels of voltage. TLC flash is 3-bit, for a total of eight possible binary states, triggered by eight different levels of voltage. TLC, therefore, allows you to store more data for the same density of flash, making it cheaper and more efficient. But there’s a downside: because each cell is being erased far more often, and has to be sensitive to more voltage levels, performance and longevity suffer compared to MLC.
+						<br /> <br />
+						<p class="page-content"
+						> While TLC has its drawbacks, memory companies are finding creative engineering solutions to minimize the performance impacts while maximizing the storage density benefits. Samsung’s 850 EVO, for example, uses a high-endurance 1-bit SLC flash cache to do most of its reading and writing, leading to great performance and plenty of longevity, since the TLC cells are only written to for longer term storage.
+							Crucial doesn’t have any SSDs to announce with its TLC NAND yet, but you can bet they’re coming—and probably with some crafty engineering work to stay competitive with Samsung.</p>
+						<br clear="left"/>
+						<p class="postline"><i style="font-size: 13px;" class="fa fa-pencil"></i> Posted by Logan Butler (Terum) on June 17th, 2015</p>
+					</div>
+					<div class="post">
 						<h2>This week’s PC gaming deals</h2>
 						<p class="sub-title">the best hardware, for the best prices I can find.</p>
 						
